67ebe10979471031f08d2f7d4cafb801e2b543f1
XavierSLN <xav.sln@gmail.com>
2016-09-13 17:31:33 +0900

    4.6 update (bis) following mentor comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
