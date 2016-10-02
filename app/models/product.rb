988dc195c02b4b71211a189d18bc0376f098511b
XavierSLN <xav.sln@gmail.com>
2016-10-02 19:14:27 +0900

    5.1: routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
 	has_many :orders
 	validates :name, presence: true
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-  root 'static_pages#landing_page'
+  root 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
