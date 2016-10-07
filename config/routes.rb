ad67b1fdc083998e20f59eac478b638a126c1001
XavierSLN <xav.sln@gmail.com>
2016-10-07 19:23:49 +0900

    5.2 Forms - Following mentor comments

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
 
   root 'static_pages#about'
+
+  post 'static_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
