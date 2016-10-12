fd61f3b28f95b44cfbffd56c42bb33ef5a0ccdb6
XavierSLN <xav.sln@gmail.com>
2016-10-12 10:33:46 +0900

    5.3 Sending mails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,5 +11,14 @@ class StaticPagesController < ApplicationController
   	@featured_product = Product.last
   end
 
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'xav.sln@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+	end
 
 end
